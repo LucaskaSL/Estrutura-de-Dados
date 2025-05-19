28a4445409fdaaf65efc7810e5dd90820ec2cfae
Lucas <lucasksleal@gmail.com>
2025-05-19 11:07:12 -0300

    Primeiro Commit

---
+++ Semana 1/treino2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <stdlib.h>
+
+//gcc treino2.c -o treino2
+
+void saudacao() {
+    printf("Ola, tudo bem?\n");
+}
+
+void despedida() {
+    printf("Ate logo!\n");
+}
+
+int main(int argc, char *argv[]) {
+int num1, num2, resultado;
+printf("\nMultiplicando valores passados na linha de comando\n");
+num1 = atoi(argv[1]); //atoi converte alfanumericos em numeros decimais
+num2 = atoi(argv[2]);
+resultado = num1 * num2;
+  printf("\n%d X %d = %d\n",num1 ,num2, resultado);
+  //getch();
+
+
+    if (argc < 2) {
+        printf("Uso: %s [saudacao|despedida]\n", argv[0]);
+        return 1;
+    }
+
+    if (strcmp(argv[3], "saudacao") == 0) {
+        saudacao();
+    } else if (strcmp(argv[3], "despedida") == 0) {
+        despedida();
+    } else {
+        printf("Comando desconhecido: %s\n", argv[3]);
+    }
+
+    return 0;
+}
